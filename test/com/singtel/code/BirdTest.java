7fb58b922835dca016cd262363e206e1bd6efa5d
Sajid Hussain <sajidhuin@gmail.com>
2020-01-21 01:52:17 +0800

    Question 1

---
+++ test/com/singtel/code/BirdTest.java
@@ -0,0 +1,33 @@
+package com.singtel.code;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BirdTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	final void test() {
+		Bird bird = new Bird();
+		bird.sing();
+	}
+
+}
