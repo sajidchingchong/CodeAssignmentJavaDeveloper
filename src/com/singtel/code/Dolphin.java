19bdb0afc7170fa8e2e88003e0263501c17dfee1
Sajid Hussain <sajidhuin@gmail.com>
2020-01-21 02:59:31 +0800

    Question B3

---
+++ src/com/singtel/code/Dolphin.java
@@ -0,0 +1,7 @@
+package com.singtel.code;
+
+public class Dolphin extends Animal {
+	Dolphin() {
+		this.canSwim = true;
+	}
+}
---
+++ test/com/singtel/code/DolphinTest.java
@@ -0,0 +1,35 @@
+package com.singtel.code;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DolphinTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	final void test() {
+		Animal dolphin = new Dolphin();
+		assertTrue(dolphin.canSwim(), "Dolphin can swim");
+	}
+
+}
