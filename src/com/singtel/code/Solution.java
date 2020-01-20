ff472bef5380ec690ecfa8981dd6311304899c47
Sajid Hussain <sajidhuin@gmail.com>
2020-01-21 05:03:40 +0800

    Counting Algorithm

--- src/com/singtel/code/Solution.java
+++ src/com/singtel/code/Solution.java
@@ -1,10 +1,51 @@
 package com.singtel.code;
 
 public class Solution {
+	private static Animal[] animals = new Animal[] { new Bird(), new Duck(), new Chicken(), new Rooster(), new Parrot(),
+			new Fish(), new Shark(), new Clownfish(), new Dolphin(), new Frog(), new Dog(), new Butterfly(),
+			new Cat() };
+
 	public static void main(String[] args) {
 		Bird bird = new Bird();
 		bird.walk();
 		bird.fly();
 		bird.sing();
+
+		System.out.println("Animals that can fly: " + countFly());
+		System.out.println("Animals that can walk: " + countWalk());
+		System.out.println("Animals that can sing: " + countSing());
+		System.out.println("Animals that can swim: " + countSwim());
+	}
+
+	private static int countFly() {
+		int count = 0;
+		for (Animal animal : animals)
+			if (animal.canFly())
+				count++;
+		return count;
+	}
+
+	private static int countWalk() {
+		int count = 0;
+		for (Animal animal : animals)
+			if (animal.canWalk())
+				count++;
+		return count;
+	}
+
+	private static int countSing() {
+		int count = 0;
+		for (Animal animal : animals)
+			if (animal.canSing())
+				count++;
+		return count;
+	}
+
+	private static int countSwim() {
+		int count = 0;
+		for (Animal animal : animals)
+			if (animal.canSwim())
+				count++;
+		return count;
 	}
 }
